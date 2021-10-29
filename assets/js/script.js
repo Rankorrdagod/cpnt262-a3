7d55615eb7907642bfa7d2e155d216b7aa47049d
Evan Burton <butonevan1@gmaail.com>
2021-10-28 22:33:13 -0600

    added drop down and bg image

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,12 +13,38 @@ fetch('https://epic.gsfc.nasa.gov/api/natural')
     // Any code that depends on the `data` must go in this block
    console.log(data);
     data.forEach(function(epic){
-    // console.log(epic);  
-    output += `<li> ID:${epic.identifier} v.${epic.version} <img src="https://epic.gsfc.nasa.gov/archive/natural/2021/10/27/jpg/${epic.image}.jpg" alt="Pictures from Oct 27th" width="275" height="275">${epic.caption} on ${epic.date}</li>`;
-
+    console.log();  
+    output += `<li> 
+    ID:${epic.identifier} v.${epic.version} <a href="https://epic.gsfc.nasa.gov/archive/natural/2021/10/27/jpg/${epic.image}.jpg">
+    <img src="https://epic.gsfc.nasa.gov/archive/natural/2021/10/27/jpg/${epic.image}.jpg" alt="Pictures from Oct 27th" width="275" height="275"></a>
+    ${epic.caption} on ${epic.date}
+    </li>`;
+   
+  
     })
     gallery.innerHTML = `<ul>${output}</ul>`;
+    
   })
+  /* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
   .catch(function(err){
     // An error or `reject` from any of the above `.then()` blocks will end up here.
     console.log(err);
